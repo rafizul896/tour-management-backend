f9e17392a9a3cfed7029aa4923f676c07ffe4fd3
rafizul896 <rafizulislam896@gmail.com>
2026-02-28 12:24:52 +0600

    add: catchAsync and sendResponse utility function

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -7,6 +7,13 @@ const createUser = async (payload: Partial<IUser>) => {
   return user;
 };
 
+const getAllUsers = async () => {
+  const users = User.find({});
+  
+  return users;
+};
+
 export const UserServices = {
   createUser,
+  getAllUsers,
 };
