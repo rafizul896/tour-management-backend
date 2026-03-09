247699d23ee67dc123f52946247d7dccb0500888
rafizul896 <rafizulislam896@gmail.com>
2026-03-09 09:36:44 +0600

    add: checkAuth middleware

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -2,44 +2,14 @@ import { Router } from "express";
 import { UserControllers } from "./user.controller";
 import validateRequest from "../../utils/validateRequest";
 import { userSchema } from "./user.validation";
-import { JwtPayload } from "jsonwebtoken";
-import { Role } from "./user.interface";
-import AppError from "../../errorHelpers/AppError";
-import httpStatus from "http-status-codes";
-import { verifyToken } from "../../utils/jwt";
-import { envVars } from "../../config/env";
-
+import checkAuth from "../../middlewares/checkAuth";
 const router = Router();
-
-const checkAuth =
-  (...authRoles: string[]) =>
-  async (req, res, next) => {
-    console.log(authRoles);
-    const accessToken = req.headers.authorization;
-    const verifiedToken = verifyToken(
-      accessToken as string,
-      envVars.JWT_ACCESS_SECRET,
-    );
-
-    if (!verifiedToken) {
-      throw new AppError(httpStatus.FORBIDDEN, "You are Not Authorized");
-    }
-
-    if ((verifiedToken as JwtPayload).role !== Role.USER) {
-      throw new AppError(
-        httpStatus.FORBIDDEN,
-        "You are Not permitted for access this route!",
-      );
-    }
-
-    next();
-  };
 
 router.post(
   "/register",
   validateRequest(userSchema),
   UserControllers.createUser,
 );
-router.get("/", checkAuth("USER","HELLO"), UserControllers.getAllUsers);
+router.get("/", checkAuth("USER"), UserControllers.getAllUsers);
 
 export const UserRoutes = router;
